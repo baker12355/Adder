2b909ba6f52f65f517a5fa8c7d8fa0f1dfaf06b3
Shao,Chun-Yang <31118891+baker12355@users.noreply.github.com>
2018-05-15 19:38:43 +0800

    update

--- load_model.py
+++ load_model.py
@@ -1,7 +1,14 @@
 from keras.models import load_model
 
+expected=[]
+thefile = open('expected.txt', 'r')
+for i in thefile:
+    expected.append(i)
 
-
+questions=[]
+thefile = open('questions.txt', 'r')
+for i in thefile:
+    questions.append(i)
 
 
 model = load_model('model.h5')
